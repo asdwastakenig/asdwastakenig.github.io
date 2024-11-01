00f53b6c4a24b4c4181a1f9b46e4f98a520adaff
asdwastakenig <164782199+asdwastakenig@users.noreply.github.com>
2024-11-01 17:08:52 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+// scroll thingy lalala!
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// animations uwa!
+const sections = document.querySelectorAll("section");
+window.addEventListener("scroll", function() {
+    sections.forEach(section => {
+        const sectionTop = section.getBoundingClientRect().top;
+        if (sectionTop < window.innerHeight * 0.75) {
+            section.classList.add("visible");
+        }
+    });
+});
